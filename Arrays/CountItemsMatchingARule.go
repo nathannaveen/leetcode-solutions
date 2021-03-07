8fc5ec350319b0fd03e1c41aa60106a142c55945
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-03-06 19:08:50 -0500

    Some golang leetcode solutions

---
+++ Arrays/CountItemsMatchingARule.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+
+}
+
+func countMatches(items [][]string, ruleKey string, ruleValue string) int {
+	res := 0
+	index := 0
+	if ruleKey == "color" {
+		index = 1
+	} else if ruleKey == "name" {
+		index = 2
+	}
+
+	for _, item := range items {
+		if item[index] == ruleValue {
+			res++
+		}
+	}
+
+	return res
+}
