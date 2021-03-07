f0021ebc81a048edb4625a6517be9a780ebe7376
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-03-07 10:46:47 -0500

    added Leetcode solution

--- Math/KClosestPointsToOrigin.go
+++ Math/KClosestPointsToOrigin.go
@@ -1,40 +1,22 @@
 package main
 
-import (
-	"math"
-)
-
 func main() {
 }
 
-func kClosest(points [][]int, K int) [][]int {
-	res := [][]int{}
+func kClosest(points [][]int, k int) [][]int {
 	for i := 1; i < len(points); i++ {
-		cur := math.Sqrt(math.Pow(float64(points[i][0]), 2) +
-			math.Pow(float64(points[i][1]), 2))
-		prev := math.Sqrt(math.Pow(float64(points[i-1][0]), 2) +
-			math.Pow(float64(points[i-1][1]), 2))
-		if cur < prev {
-			p := points[i]
-			isReplaced := false
-			for j := i - 1; j >= 0; j-- {
-				points[j+1] = points[j]
-				if cur > math.Sqrt(math.Pow(float64(points[j][0]), 2)+
-					math.Pow(float64(points[j][1]), 2)) {
-					points[j] = p
-					i = j
-					isReplaced = true
-					break
-				}
-			}
-			if !isReplaced {
-				points[0] = p
-				i = 1
+		if i >= 1 {
+			distanceOfIMinusOne := square(points[i-1][0]) + square(points[i-1][1])
+			distanceOfI := square(points[i][0]) + square(points[i][1])
+			if distanceOfIMinusOne > distanceOfI {
+				points[i-1], points[i] = points[i], points[i-1]
+				i -= 2
 			}
 		}
 	}
-	for i := 0; i < K; i++ {
-		res = append(res, points[i])
-	}
-	return res
+	return points[:k]
+}
+
+func square(n int) int {
+	return n * n
 }
