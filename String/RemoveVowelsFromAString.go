6d0f4451f25a2b6a8a78a87876dd104c33ebf8c3
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-03-18 12:34:46 -0400

    added more Leetcode solutions

---
+++ String/RemoveVowelsFromAString.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+}
+
+func removeVowels(s string) string {
+	for i := 0; i < len(s); i++ {
+		if s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' {
+			s = s[:i] + s[i+1:]
+			i -= 1
+		}
+	}
+	return s
+}
