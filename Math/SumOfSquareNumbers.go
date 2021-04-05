604457ef8ec52054dbb7b5d1d8a4eb581449e19c
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-04-05 10:38:01 -0500

    added more Leetcode solutions

---
+++ Math/SumOfSquareNumbers.go
@@ -0,0 +1,20 @@
+package main
+
+import "math"
+
+func judgeSquareSum(c int) bool {
+	for i := 0; i*i <= c; i++ {
+		if isSquare(c - (i * i)) {
+			return true
+		}
+	}
+	return false
+}
+
+func isSquare(n int) bool {
+	squareRooted := int(math.Sqrt(float64(n)))
+	if squareRooted*squareRooted == n {
+		return true
+	}
+	return false
+}
