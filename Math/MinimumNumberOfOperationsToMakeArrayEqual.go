a2b36016e6982be21ae7de0d0edb532a3d393d70
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-04-11 04:09:39 -0500

    added more Leetcode solutions

--- Math/MinimumNumberOfOperationsToMakeArrayEqual.go
+++ Math/MinimumNumberOfOperationsToMakeArrayEqual.go
@@ -6,8 +6,10 @@ func main() {
 
 func minOperations(n int) int {
 	if n%2 == 1 {
-		return (n >> 1) * ((n >> 1) + 1)
+		n /= 2
+		return n * (n + 1)
 	} else {
-		return (n >> 1) * (n >> 1)
+		n /= 2
+		return n * n
 	}
 }
