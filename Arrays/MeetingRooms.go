4de99a7de143b6e6d30cd40dcb16b6396957bf3d
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-04-13 09:25:15 -0400

    added more Leetcode solutions

---
+++ Arrays/MeetingRooms.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+}
+
+func canAttendMeetings(intervals [][]int) bool {
+	for i := 1; i < len(intervals); i++ {
+		if i >= 1 && intervals[i-1][0] > intervals[i][0] {
+			intervals[i-1], intervals[i] = intervals[i], intervals[i-1]
+			i -= 2
+		}
+	}
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i-1][1] > intervals[i][0] {
+			return false
+		}
+	}
+	return true
+}
