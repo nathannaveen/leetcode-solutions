25934b0d6fce88ba23e4a5befff139b87348ee03
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-04-15 09:40:02 -0400

    added more Leetcode solutions

---
+++ Math/SignOfProductInArray.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+
+}
+
+func arraySign(nums []int) int {
+	sign := 1
+
+	for _, num := range nums {
+		if num <= -1 {
+			sign = -sign
+		} else if num == 0 {
+			return 0
+		}
+	}
+
+	return sign
+}
