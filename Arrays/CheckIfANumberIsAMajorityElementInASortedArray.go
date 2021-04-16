70979b809ca4e26a468d9919b65e2c5e65a11bcb
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-04-16 18:51:12 -0400

    added more Leetcode solutions

---
+++ Arrays/CheckIfANumberIsAMajorityElementInASortedArray.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+
+}
+
+func isMajorityElement(nums []int, target int) bool {
+	m := make(map[int]int)
+
+	for _, num := range nums {
+		m[num]++
+	}
+	return m[target] > len(nums)/2
+}
