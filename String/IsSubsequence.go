60780877e0fc42a7c9a17bb48a3dff5c072c1694
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-04-17 22:19:19 -0400

    added more Leetcode solutions

---
+++ String/IsSubsequence.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+}
+
+func isSubsequence(s string, t string) bool {
+	sCounter, i := 0, 0
+
+	for i < len(t) && sCounter < len(s) {
+		if t[i] == s[sCounter] {
+			sCounter++
+		}
+		i++
+	}
+	return sCounter == len(s)
+}
