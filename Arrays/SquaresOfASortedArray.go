3349bd7fe4b4dad58bb258857ae36af1d10eb853
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-04-19 10:48:41 -0400

    added more Leetcode solutions

--- Arrays/SquaresOfASortedArray.go
+++ Arrays/SquaresOfASortedArray.go
@@ -5,17 +5,14 @@ func main() {
 }
 
 func sortedSquares(nums []int) []int {
-	left, right := 0, len(nums)-1
-	res := []int{}
-
-	for left <= right {
-		l, r := nums[left]*nums[left], nums[right]*nums[right]
-		if l > r {
-			res, left = append([]int{l}, res...), left+1
-		} else {
-			res, right = append([]int{r}, res...), right-1
+	for i := 0; i < len(nums); i++ {
+		nums[i] = nums[i] * nums[i]
+	}
+	for i := 1; i < len(nums); i++ {
+		if i >= 1 && nums[i-1] > nums[i] {
+			nums[i-1], nums[i] = nums[i], nums[i-1]
+			i -= 2
 		}
 	}
-
-	return res
+	return nums
 }
