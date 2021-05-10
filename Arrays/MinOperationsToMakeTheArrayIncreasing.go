50518b01c30d21529566fd822d9a92c2faf7eed4
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-05-09 19:16:01 -0500

    added more Leetcode solutions

--- Arrays/MinOperationsToMakeTheArrayIncreasing.go
+++ Arrays/MinOperationsToMakeTheArrayIncreasing.go
@@ -6,10 +6,9 @@ func main() {
 
 func minOperations(nums []int) int {
 	res := 0
-	previous := nums[0]
 	for i := 1; i < len(nums); i++ {
-		res += max(0, previous+1-nums[i])
-		previous = max(nums[i], nums[i-1]+1)
+		res += max(0, nums[i-1]+1-nums[i])
+		nums[i] = max(nums[i], nums[i-1]+1)
 	}
 	return res
 }
