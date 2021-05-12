c0025734c67e1eab9b813dc615e8e18def478c31
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-05-12 06:22:15 -0500

    added more Leetcode solutions

--- Arrays/MinimumNumberOfIncrementsOnSubarraysToFormATargetArray.go
+++ Arrays/MinimumNumberOfIncrementsOnSubarraysToFormATargetArray.go
@@ -1,30 +1,18 @@
 package main
 
-import "sort"
-
 func main() {
 
 }
 
 func minNumberOperations(target []int) int {
 	res := 0
-	subtract := 0
-	counter := 0
-	h := []int{}
 
-	for _, i := range target {
-		h = append(h, i)
-	}
-	sort.Ints(h)
-
-	for _, i2 := range target {
-		if i2-subtract <= 0 && counter > 0 {
-			counter = 0
-			res++
-		} else if i2-subtract > 0 {
-
+	for i := 1; i < len(target); i++ {
+		temp := target[i] - target[i-1]
+		if temp > 0 {
+			res += temp
 		}
 	}
 
-	return res
+	return res + target[0]
 }
