95964e97c9d501e89316e7c218c7d22077f75b95
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-05-23 12:17:58 -0400

    added more Leetcode solutions

--- LinkedList/MergeTwoSortedLists.go
+++ LinkedList/MergeTwoSortedLists.go
@@ -5,19 +5,47 @@ func main() {
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	res := &ListNode{}
-	for true {
-		if l1 != nil && l1.Next != nil && l2 != nil && l2.Next != nil {
-			if l1.Val <= l2.Val {
-				res = l1
-				res = res.Next
-				l1 = l1.Next
-			}
-			if l2.Val > l1.Val {
-				res = l2
-			}
-		}
-		break
+	if l1 == nil && l2 == nil {
+		return l1
+	} else if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
 	}
-	return res
+
+	l := &ListNode{}
+	l1, l2, l = maxAndExtract(l1, l2, l)
+	head := l
+
+	for l1 != nil && l2 != nil {
+		l1, l2, l = maxAndExtract(l1, l2, l)
+	}
+
+	for l1 != nil {
+		l.Next = l1
+		l = l.Next
+		l1 = l1.Next
+	}
+
+	for l2 != nil {
+		l.Next = l2
+		l = l.Next
+		l2 = l2.Next
+	}
+
+	return head
+}
+
+func maxAndExtract(l1, l2, l *ListNode) (*ListNode, *ListNode, *ListNode) {
+	if l1.Val > l2.Val {
+		l.Next = l2
+		l = l.Next
+		l2 = l2.Next
+		return l1, l2, l
+	}
+
+	l.Next = l1
+	l = l.Next
+	l1 = l1.Next
+	return l1, l2, l
 }
