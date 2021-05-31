6b4df8b6eddda0fe5659b2414483cba7bf19d35f
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2021-05-31 08:00:56 -0500

    added more Leetcode solutions

---
+++ Arrays/BulbSwitcherThree.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+}
+
+func numTimesAllBlue(light []int) int {
+	numOnLeft := 0
+	res := 0
+
+	for i := 0; i < len(light); i++ {
+		if light[i] > numOnLeft {
+			numOnLeft = light[i]
+		}
+		if numOnLeft == i+1 {
+			res++
+		}
+	}
+	return res
+}
